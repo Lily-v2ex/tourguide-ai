a4c7a66570bff1ba55dc5b2d90be0cadd93a6ba1
lily_v2ex <lily_v2ex@u.circlecat.org>
2024-10-17 10:13:56 +0800

    [TGA-17] Modified classify_input function in langchain_tools.py to implement AI reponse to scenic spot questions.
    Change-Id: If081f0e1d31f036e80a91b3145c081f0de7b9a21
    Reviewed-on: http://review.circlecat.org/c/tourguide-ai/+/4734
    Tested-by: Presubmit <CatBot@u.circlecat.org>
    LGTM: Xi Liu <xifutongliu@u.circlecat.org>
    LGTM: 李 丽仪 <lily_v2ex@u.circlecat.org>
    Approval: Moira Zheng <moiraz@u.circlecat.org>
    Approval: Xi Liu <xifutongliu@u.circlecat.org>
    LGTM: Moira Zheng <moiraz@u.circlecat.org>

--- tools/langchain_tools.py
+++ tools/langchain_tools.py
@@ -20,6 +20,7 @@ from langchain_community.vectorstores.faiss import FAISS
 from constants import ATTRACTION_SYSTEM_MESSAGE
 from constants import OTHER_SYSTEM_MESSAGE
 from constants import BUILDING_SYSTEM_MESSAGE
+from constants import SCENERY_SYSTEM_MESSAGE
 
 
 def set_api_key(keyword: str):
@@ -37,6 +38,7 @@ set_api_key("SERPAPI_API_KEY")
 msg_dict = {
     "Attraction": ATTRACTION_SYSTEM_MESSAGE,
     "Building": BUILDING_SYSTEM_MESSAGE,
+    "Scenery": SCENERY_SYSTEM_MESSAGE,
     "Other": OTHER_SYSTEM_MESSAGE
 }
 
@@ -60,11 +62,15 @@ def classify_input(user_input: str, chat_history: list) -> str:
                    "Classify the latest user input into the following groups: "
                    "'Attraction' "
                    "'Building' "
+                   "'Scenery' "
                    "and 'Other' "
                    "by the below rules."
                    "If the latest user input is a building in the last attraction "
                    "mentioned in the chat history, "
                    "then return 'Building'."
+                   "Else if the latest user input is a scenic spot "
+                   "in the last attraction mentioned in the chat history, "
+                   "then return 'Scenery'."
                    "Else If the latest user input is the very first famous attraction"
                    "mentioned in the chat history "
                    "or it is a new attraction that was not in the same location "
@@ -87,6 +93,11 @@ def classify_input(user_input: str, chat_history: list) -> str:
                    "[User]: the Mingshan Hall"
                    "EXAMPLE OUTPUT"
                    "Building"
+                   "EXAMPLE CONVERSATION"
+                   "[User]: Jiuzhaigou"
+                   "[AI]: Attraction"
+                   "[User]: five flower lake"
+                   "[AI]: Scenery"
                    "EXAMPLE CONVERSATION"
                    "[User]: Qingjing Mosque"
                    "[User]: the Mingshan Hall"
